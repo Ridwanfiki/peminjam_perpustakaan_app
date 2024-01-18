62ce455b3a7e76cccd3e2264149146ce6b6834e2
RayhanRistyawan <rayhanristyawan@gmail.com>
2024-01-18 13:06:43 +0700

    book page

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,7 +1,19 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:dio/dio.dart' as dio;
+import 'package:peminjam_perpustakaan_app/app/data/constant/endpoint.dart';
+import 'package:peminjam_perpustakaan_app/app/data/model/response_login.dart';
+import 'package:peminjam_perpustakaan_app/app/data/provider/api_provider.dart';
+import 'package:peminjam_perpustakaan_app/app/data/provider/storage_provider.dart';
+import 'package:peminjam_perpustakaan_app/app/routes/app_pages.dart';
 
 class LoginController extends GetxController {
   //TODO: Implement LoginController
+  final GlobalKey<FormState> formKey= GlobalKey<FormState>();
+  final TextEditingController usernameController= TextEditingController();
+  final TextEditingController passwordController= TextEditingController();
 
   final count = 0.obs;
   @override
@@ -12,6 +24,11 @@ class LoginController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+    String status = StorageProvider.read(StorageKey.status);
+    log("status : $status");
+    if(status == "logged"){
+      Get.offAllNamed(Routes.HOME);
+    }
   }
 
   @override
@@ -20,4 +37,42 @@ class LoginController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  final loadingLogin = false.obs;
+  Login() async {
+    loadingLogin(true);
+    try {
+      FocusScope.of(Get.context!).unfocus();
+      formKey.currentState?.save();
+      if (formKey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.login,
+            data: dio.FormData.fromMap(
+                {"username": usernameController.text.toString(), "password": passwordController.text.toString()}));
+        if (response.statusCode == 200) {
+          final ResponseLogin responseLogin =
+          ResponseLogin.fromJson(response.data);
+          await StorageProvider.write(StorageKey.idUser,
+              "${responseLogin.data?.id}");
+          await StorageProvider.write(StorageKey.status, "logged");
+          Get.offAllNamed(Routes.HOME);
+        } else {
+          Get.snackbar("Sorry", "Login Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loadingLogin(false);
+    } on dio.DioException catch (e) {
+      loadingLogin(false);
+      if(e.response?.data!=null) {
+        final ResponseLogin responseLogin =
+        ResponseLogin.fromJson(e.response!.data);
+        Get.snackbar("Sorry", responseLogin.message??"error", backgroundColor: Colors.red);
+      } else {
+        Get.snackbar("Sorry", e.message ?? "", backgroundColor: Colors.red);
+      }
+    } catch (e) {
+      loadingLogin(false);
+      Get.snackbar("Error", e.toString(), backgroundColor: Colors.red);
+      throw Exception('Error: $e');
+    }
+  }
 }
