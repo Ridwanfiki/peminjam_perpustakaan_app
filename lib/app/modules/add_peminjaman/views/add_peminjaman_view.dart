59784af439f67f263aaae3a552a6b7ff72255545
RayhanRistyawan <rayhanristyawan@gmail.com>
2024-01-18 15:30:41 +0700

    peminjaman page

--- lib/app/modules/add_peminjaman/views/add_peminjaman_view.dart
+++ lib/app/modules/add_peminjaman/views/add_peminjaman_view.dart
@@ -13,11 +13,39 @@ class AddPeminjamanView extends GetView<AddPeminjamanController> {
         title: const Text('AddPeminjamanView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'AddPeminjamanView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Center(
+          child: Form(
+            key: controller.formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: controller.tglPinjamController,
+                  decoration: InputDecoration(hintText: "Masukkan Tanggal Pinjam"),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Tanggal Pinjam tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: controller.tglKembaliController,
+                  decoration: InputDecoration(hintText: "Masukkan Tanggal Kembali"),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Tanggal Kembali tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                ),
+                Obx(() {
+                  return controller.loading.value
+                      ? const CircularProgressIndicator()
+                      : ElevatedButton(onPressed: () => controller.post(), child: const Text("Pinjam"));
+                }).paddingOnly(top: 16)
+              ],
+          ).paddingOnly(left: 16, right: 16),
+      )
       ),
     );
   }
